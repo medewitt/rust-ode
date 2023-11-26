4b90e3bd96541159f04b757d2288fda9a6df50f7
Michael DeWitt <me.dewitt.jr@gmail.com>
2023-11-26 08:19:01 -0500

    testing verification

--- graphy.R
+++ graphy.R
@@ -1,18 +1,23 @@
 library(tidyverse)
+library(patchwork)
 
+
+# Data from solver
 dat <- readr::read_csv(file.path("outputs", "output.dat"), col_names = c("time","S", "I","R", "Re"))
 
 nrow(dat)
 
+# My themes
 theme_set(theme_classic(base_size = 14))
 
-
+# Looks at each compartment
 fig_a <- dat |>
 gather(compartment, value, -time) |>
 ggplot(aes(time/365, value, color = compartment))+
 geom_line()+
 theme(legend.position = "top")
 
+# Look at the Rt over time in a very crude way
 fig_b <- dat |>
 mutate(Re = S/(S+I+R)*(2.5)) |>
 ggplot(aes(time/365, Re))+
@@ -21,12 +26,7 @@ theme(legend.position = "top")+
 geom_hline(yintercept = 1, lwd = 1.1, lty = 2, color = "tomato" )+
 scale_y_continuous(name = "Effective Reproduction Number", limits = c(0,NA), expand = c(0,0))
 
-library(patchwork)
-
+# Make something to look at
 png("assets/graph.png")
 fig_a /fig_b
 dev.off()
-
-1/10
-
-2.5
